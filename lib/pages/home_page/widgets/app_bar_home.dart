3935c71a9cccb920692ced0352274570cb19b837
Rafael Neves <rafael@giver.com.br>
2020-07-02 14:02:49 -0300

    First commit

---
+++ lib/pages/home_page/widgets/app_bar_home.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AppBarHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 13, right: 5),
+                  child: IconButton(
+                    icon: Icon(Icons.menu),
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Text(
+                  'Pokedex',
+                  style: TextStyle(
+                    fontFamily: 'Google',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+      height: 120,
+      // color: Color.fromARGB(200, 240, 245, 210),
+    );
+  }
+}
