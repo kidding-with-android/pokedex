ad4681718ed5869e439eaa7211408b571ffd7744
Rafael Neves <rafael@giver.com.br>
2020-07-06 13:37:49 -0300

    Aula 2

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:pokedex/consts/consts_app.dart';
+import 'package:pokedex/models/pokeapi.dart';
 import 'package:pokedex/pages/home_page/widgets/app_bar_home.dart';
+import 'package:pokedex/stores/pokeapi_store.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    PokeApiStore pokeApiStore = PokeApiStore();
+    pokeApiStore.fetchPokemonList();
+
     double screenWidth = MediaQuery.of(context).size.width;
     double statusHeight = MediaQuery.of(context).padding.top;
 
@@ -35,10 +41,23 @@ class HomePage extends StatelessWidget {
                 AppBarHome(),
                 Expanded(
                   child: Container(
-                    child: ListView(
-                      children: [
-                        ListTile(title: Text('Pokemon')),
-                      ],
+                    child: Observer(
+                      name: 'ListaHomePage',
+                      builder: (BuildContext context) {
+                        PokeAPI _pokeAPI = pokeApiStore.pokeAPI;
+                        return (_pokeAPI != null)
+                            ? ListView.builder(
+                                itemCount: _pokeAPI.pokemon.length,
+                                itemBuilder: (context, index) {
+                                  return ListTile(
+                                    title: Text(_pokeAPI.pokemon[index].name),
+                                  );
+                                },
+                              )
+                            : Center(
+                                child: CircularProgressIndicator(),
+                              );
+                      },
                     ),
                   ),
                 ),
---
+++ lib/stores/pokeapi_store.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:mobx/mobx.dart';
+import 'package:pokedex/consts/consts_api.dart';
+import 'package:pokedex/models/pokeapi.dart';
+part 'pokeapi_store.g.dart';
+
+class PokeApiStore = _PokeApiStoreBase with _$PokeApiStore;
+
+abstract class _PokeApiStoreBase with Store {
+  @observable
+  PokeAPI pokeAPI;
+
+  @action
+  fetchPokemonList() {
+    pokeAPI = null;
+    loadPokeAPI().then((pokeList) => {
+      pokeAPI = pokeList
+    });
+  }
+
+  Future<PokeAPI> loadPokeAPI() async {
+    try {
+      final response = await http.get(ConstsAPI.pokeapiURL);
+      var decodeJson = jsonDecode(response.body);
+      return PokeAPI.fromJson(decodeJson);
+    } catch (error, stacktrace) {
+      print("Erro ao carregar lista");
+      return null;
+    }
+  }
+}
